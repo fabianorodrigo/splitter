0ecca00c563c47c8309de41a9282ba6c468fbe5e
Hilmar Orth <hm.orth@aol.com>
2019-06-02 12:39:51 +0200

    removed SafeMath fron migration file

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -2,6 +2,5 @@ const SafeMath = artifacts.require('SafeMath');
 const Splitter = artifacts.require('Splitter');
 
 module.exports = function(deployer, network, accounts) {
-    deployer.deploy(SafeMath);
     deployer.deploy(Splitter, accounts[1], accounts[2], {from: accounts[0]});
 }
