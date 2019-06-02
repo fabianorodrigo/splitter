ba68315a36ccc08436e7a6ec2ea5fdef59c306d0
Hilmar Orth <hm.orth@aol.com>
2019-06-02 12:37:30 +0200

    added const & let to variables in unit test

--- test/splitter.js
+++ test/splitter.js
@@ -4,16 +4,16 @@ const SafeMath = artifacts.require('SafeMath')
 contract('Splitter', (accounts) => {
     it('balance of contract after sending 1 eth to split function should equal msg.value', async () => {
         // Set up 3 test accounts
-        account1 = accounts[0];
-        account2 = accounts[1];
-        account3 = accounts[2];
+        let account1 = accounts[0];
+        let account2 = accounts[1];
+        let account3 = accounts[2];
 
-        safemath = await SafeMath.deployed();
-        splitter = await Splitter.deployed(account2, account3, {from: account1});
+        const safemath = await SafeMath.deployed();
+        const splitter = await Splitter.deployed(account2, account3, {from: account1});
 
-        messageValue = await splitter.splitEther( {from: account1, value: web3.utils.toWei("1")} );
+        const messageValue = await splitter.splitEther( {from: account1, value: web3.utils.toWei("1")} );
 
-        contractBalance = await splitter.getContractBalance();
+        const contractBalance = await splitter.getContractBalance();
 
         assert.equal(web3.utils.toWei("1"), contractBalance, "Value of message sent should be equal to the contracts balance");
     })
